fcc06c843636208114179ccc8204f318db5feb8b
latz09 <85132328+latz09@users.noreply.github.com>
2026-08-24 21:06:42 -0500

    d

--- components/sections/in-stock-rods/RodSeriesHero.js
+++ components/sections/in-stock-rods/RodSeriesHero.js
@@ -5,7 +5,7 @@ const RodSeriesHero = ({ data }) => {
 	const { applicationType, description, heroImage, name, slug } = data;
 	return (
 		<div className='section-x-padding  mt-1 lg:mt-2 mb-5 '>
-			<div className="px-1.25 lg:px-4 bg-gradient-to-t from-accent/50 via-accent/0 to-accent/0 rounded  pb-2.5 lg:pb-5">
+			<div className="lg:px-4 bg-gradient-to-t from-accent/50 via-accent/0 to-accent/0 rounded  pb-2.5 lg:pb-5">
 				<Link
 					href='/shop/rods'
 					className='text-dark text-paragraph-sm font-[500] hover:text-primary transition duration-300'
@@ -13,7 +13,7 @@ const RodSeriesHero = ({ data }) => {
 					In stock rods /
 				</Link>
 				<div className=' grid lg:grid-cols-2 gap-1.5 lg:gap-5.5 mt-1'>
-					<div className='order-2 lg:order-1 space-y-1.25 lg:space-y-1.5'>
+					<div className='order-2 lg:order-1 space-y-1.25 lg:space-y-1.5 px-1.25 lg:px-0'>
 						<h1>{name}</h1>
 						<p className='text-paragraph-lg'>{description}</p>
 					</div>
